c14e8c8671cef8287685840fd7f642939f1b7339
oma16 <titioba95@gmail.com>
2021-04-19 00:08:48 +0100

    first commit

---
+++ fetchtask.js
@@ -0,0 +1,27 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+const path = require('path');
+const dir = './result';
+
+   // fetching the data from placeholder
+let url = "https://jsonplaceholder.typicode.com/posts";
+
+let settings = { method: "Get" };
+
+fetch(url, settings)
+    .then(res => res.json())
+    .then((json) => {
+       // creating directory "result"
+fs.mkdir(dir,(err)=> {
+  if(err){
+    throw err;
+  }
+  console.log("Directory is created.");
+});
+       // writing to the file posts.json  
+fs.writeFile('./result/posts.json',JSON.stringify(json), function (err) {
+  if (err) return console.log(err);
+  console.log(JSON.stringify(json) > 'posts.json');
+
+});
+    });
